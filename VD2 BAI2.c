cb2786f144f18279e78dc2a309ff6d75bc2e640a
ngodat2002 <73330656+ngodat2002@users.noreply.github.com>
2020-10-23 14:26:13 +0700

    lab01

---
+++ VD2 BAI2.c
@@ -0,0 +1,9 @@
+main()
+{
+	char abc; /*abc of type character */
+	int xyz; /*xyz of type integers */
+	float length; /*length of type float*/
+	double area; /*liteyrs of type long int*/
+	long liteyrs; /*liteyrs of type long int */
+	short arm; /*arm of type short integer*/
+}
