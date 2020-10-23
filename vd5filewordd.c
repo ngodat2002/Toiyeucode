0c28acff2b05eca66a0dcca4a4c89f23ad05c4e0
ngodat2002 <73330656+ngodat2002@users.noreply.github.com>
2020-10-23 14:18:56 +0700

    vd5fileword

---
+++ vd5filewordd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+	float r;
+	float h;
+	float c;
+	printf("Nhap so de tinh the tich hinh tru\n");
+	scanf("%f",&r);
+		printf("Nhap so de tinh the tich hinh tru\n");
+	scanf("%f",&h);
+	
+	
+	c = r*r*h*3,14;
+	printf("%f*%f*3,14=%f",r,h,c);
+	
+	
+}
