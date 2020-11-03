656e0315241e69bcc3353ff21cc61d651262e691
ngodat2002 <73330656+ngodat2002@users.noreply.github.com>
2020-11-03 23:16:33 +0700

    lab10

---
+++ lab10/bai16code1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+	printf("\n I am in main");
+	italy();
+	brazil();
+	argentina();
+	
+}
+italy(){
+	printf("I am in italy\n");
+}
+brazil()
+{
+	printf("I am in brazil\n");
+}
+argentina()
+{
+	printf("I am in argentina\n");
+}
---
+++ lab10/bai16code2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,sum;
+	printf("\n Enter any there numbers:");
+	scanf("%d %d %d",&a,&b,&c);
+	sum= calculatesum(a,b,c);
+	printf("\n Sum=%d",sum);
+}
+calculatesum(int x,int y, int z)
+{
+	int d;
+	d=x+y+z;
+	return(d);
+}
