cf806b06c83c06e4f26275aa25d654944341aad2
ngodat2002 <73330656+ngodat2002@users.noreply.github.com>
2021-01-21 23:46:53 +0700

    TASK3

---
+++ SQLLTASK3/bai2.sql
@@ -0,0 +1,90 @@
+﻿use example5
+go
+create table booklibary(
+BookCode int primary key,
+BookTitle varchar(100),
+Author varchar(50),
+Edition int,
+BookPrice money,
+Copies int,
+)
+go
+select * from booklibary
+go
+--tao insert
+insert into booklibary(BookCode,BookTitle,Author,Edition,BookPrice,Copies) values(101,'Hello My Friend','Meis',2020,$120,5)
+insert into booklibary(BookCode,BookTitle,Author,Edition,BookPrice,Copies) values(302,'My dream  me with you','Niso',1953,$10,10)
+insert into booklibary(BookCode,BookTitle,Author,Edition,BookPrice,Copies) values(150,'I Love You','Shir',1996,$50,15)
+insert into booklibary(BookCode,BookTitle,Author,Edition,BookPrice,Copies) values(942,'I hate you','Nger',2021,$68,4)
+insert into booklibary(BookCode,BookTitle,Author,Edition,BookPrice,Copies) values(845,'Book Limited','Me',2021,$12,1)
+go
+--tao bang member 
+create table member(
+MemBerCode int primary key,
+Name nvarchar(50),
+Address varchar(100),
+PhoneNumber int,
+)
+go
+select * from member
+go
+--tao insert
+insert into member(MemBerCode,Name,Address,PhoneNumber) values (202,'Jhon Wick','New York',0394480757)
+insert into member(MemBerCode,Name,Address,PhoneNumber) values (451,'Captian','Endland',548116962)
+insert into member(MemBerCode,Name,Address,PhoneNumber) values (102,'Batman','New York',364413584)
+insert into member(MemBerCode,Name,Address,PhoneNumber) values (364,'Superman','New York',79621651)
+insert into member(MemBerCode,Name,Address,PhoneNumber) values (597,'Thor','Taiwan',11219999)
+go
+--tao bang thong tin muon sach
+create table issuedetails(
+IssueDate datetime,
+ReturnDate datetime,
+BookCode int,
+MemBerCode int,
+constraint ml foreign key (BookCode) references booklibary(BookCode),
+constraint mk foreign key (MemBerCode) references member(MemBerCode)
+)
+go
+select * from issuedetails
+go
+
+--tao insert 
+insert into issuedetails(IssueDate,ReturnDate,BookCode,MemBerCode) values (2012/12/12,2012/12/12,101,202)
+insert into issuedetails(IssueDate,ReturnDate,BookCode,MemBerCode) values (2012/12/12,2012/12/12,302,451)
+insert into issuedetails(IssueDate,ReturnDate,BookCode,MemBerCode) values (2012/12/12,2012/12/12,150,102)
+insert into issuedetails(IssueDate,ReturnDate,BookCode,MemBerCode) values (2012/12/12,2012/12/12,942,364)
+insert into issuedetails(IssueDate,ReturnDate,BookCode,MemBerCode) values (2012/12/12,2012/12/12,845,597)
+go
+--xoa bang dl
+drop table issuedetails
+go
+drop table booklibary
+go
+drop table member
+go
+
+--Xóa b? các ràng bu?c khóa ngo?i c?a b?ng IssueDetails
+Alter table issuedetails drop constraint ml
+go
+Alter table issuedetails drop constraint mk
+go
+--Xóa b? ràng bu?c khóa chính c?a b?ng member và book
+Alter table booklibary drop constraint PK_BookCode
+Alter table member drop constraint PK_MemBerCode
+go
+--thêm mới ràng buộc khóa chính cho member và booklibary
+alter table booklibary add constraint pk_book primary key(Author)
+alter table member add constraint pk_mem primary key(Address)
+--Bổ sung thêm Ràng buộc giá bán sách > 0 và < 200
+ALTER TABLE booklibary 
+ADD CONSTRAINT BookPrice CHECK (>0 AND <200);
+go
+--Bổ sung thêm Ràng buộc duy nhất cho PhoneNumber của bảng Member
+alter table member add constraint pk_mer primary key(PhoneNumber)
+--Bổ sung thêm ràng buộc NOT NULL cho BookCode, MemberCode trong bảng IssueDetails
+ALTER TABLE issuedetails MODIFY BookCode int datatype NOT NULL;
+--Tạo khóa chính gồm 2 cột BookCode, MemberCode cho bảng IssueDetails
+ALTER TABLE issueDetails 
+ADD CONSTRAINT MyPrimaryKey PRIMARY KEY (BookCode,MemBerCode);
+go
+--Chèn dữ liệu hợp lý cho các bảng(Sử dụng SQL)
---
+++ SQLLTASK3/bài1.sql
@@ -0,0 +1,90 @@
+﻿USE AdventureWorks2019
+GO
+if exists (select * from sys.databases where name like 'example5') Drop database example5
+go
+
+create database example5
+go
+use example5
+go
+
+create table LopHoc(
+MaLopHoc int primary key identity,
+TenLopHoc varchar(10)
+)
+go
+
+
+create table SinhVien(
+MaSinhVien int primary key,
+TenSinhVien nvarchar(40),
+MaLopHoc int,
+constraint ml foreign key (MaLopHoc) references LopHoc(MaLopHoc)
+)
+go
+--tao bang san pham voi mot cot null, mot cot not null
+create table sanpham (
+masp int not null,
+tensp varchar(40) null
+)
+go
+--tao bang thuoc tinh default cho cot price
+create table StoreProduct(
+ProductID int NOT NULL,
+Name varchar(40) NOT NULL,
+price money not null default (100)
+)
+--thu kiem tra xem gia tri default co duoc su dung hay khong
+insert into StoreProduct (ProductID,Name) values (111, 'Rivets')
+go
+--tao bang bang contactphone voi thuoc tinh identity
+create table Contactphone(
+Person_ID int IDENTITY(500,1) not null,
+MobileNumber bigint not null
+
+)
+go
+--tao cot nhan dang duy nhat tong the
+create table CellularPhone(
+Person_ID uniqueidentifier DEFAULT NEWID () not null,
+personName varchar(60) not null
+)
+--chen dong record vao
+insert into CellularPhone(personName) values('William Smith')
+go
+--kiem tra gia tri cua cot person_id tu dong sinh
+select * from CellularPhone
+go
+--tao bang  contactphone voi cot moblienumber co thuoc tinh unique
+create table ContactPhone (
+Person_ID int primary key,
+mobileNumber bigint unique,
+ServiceProvider varchar(30),
+LandlineNumber bigint unique
+)
+--chen 2 ban ghi co gia tri giong nhau o cot mobilenumber va landlinenumber de quan sat loi
+insert into Contactphone values(101, 983345674,'Hutch',null)
+insert into Contactphone values(101, 983345674,'Alex',null)
+Go
+--Tạo bảng PhoneExpenses có một CHECT ở cột Amount
+CREATE TABLE PhoneExpenses (
+Expense_ID int PRIMARY KEY,
+MobileNumber bigint FOREIGN KEY REFERENCES ContactPhone
+(MobileNumber),
+Amount bigint CHECK (Amount >0)
+)
+GO
+--Chỉnh sửa cột trong bảng
+ALTER TABLE ContactPhone
+ALTER COLUMN ServiceProvider varchar(45)
+GO
+--Xóa cột trong bảng
+ALTER TABLE ContactPhone
+DROP COLUMN ServiceProvider
+GO
+---Them một ràng buộc vào bảng
+ALTER TABLE ContactPhone ADD CONSTRAINT CHK_RC CHECK(RentalCharges >0)
+GO
+--Xóa một ràng buộc
+ALTER TABLE Person.ContactPhone
+DROP CONSTRAINT CHK_RC
